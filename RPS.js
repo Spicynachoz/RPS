37b0f1400a0705bd70e72c1177e5ebd6ff8a6cc8
Spicynachoz <davidye93@gmail.com>
2023-05-22 12:41:54 -0400

    Final commit
    Included running score tracker and final score tracker.

--- RPS.js
+++ RPS.js
@@ -2,13 +2,38 @@
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissor');
-const outcomeDiv = document.querySelector('.outcome')
+const outcomeDiv = document.querySelector('.outcome');
+const runningDiv = document.querySelector('.runScore');
 
+const checkForWinner = (playerScore, computerScore) => {
+    // compare first score to 5 and print out on a seperate headerline
+    if (playerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.classList.add('player-won')
+        h2.innerText = `GG, you won! Final Score : Player: ${playerScore} to Computer :${computerScore}`;
+        outcomeDiv.append(h2);
+        
+        //append h2 to outcomeDiv for finalScore. use backtick ` to implement inline element 
+        
+
+    }
+    if(computerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.classList.add('computer-won');
+        h2.innerText = `GG, you lost! Final score : Computer : ${computerScore} to Player ${playerScore}`;
+        outcomeDiv.append(h2);
+
+    }
+    
+}
+
+//create buttons and event listeners for click selection
 rockButton.addEventListener('click',() =>{
 
     const computerSelection = getComputerChoice()
     const playerSelection = 'rock';
     playRound(playerSelection,computerSelection)
+    checkForWinner(playerScore, computerScore);
 } )
 
 paperButton.addEventListener('click',() =>{
@@ -16,6 +41,7 @@ paperButton.addEventListener('click',() =>{
     const computerSelection = getComputerChoice()
     const playerSelection = 'paper';
     playRound(playerSelection,computerSelection)
+    checkForWinner(playerScore, computerScore);
 } )
 
 scissorsButton.addEventListener('click',() =>{
@@ -23,9 +49,17 @@ scissorsButton.addEventListener('click',() =>{
     const computerSelection = getComputerChoice()
     const playerSelection = 'scissor';
     playRound(playerSelection,computerSelection)
+    checkForWinner(playerScore, computerScore);
 } )
 
 
+//added another div to record running score 
+const p1 = document.createElement('p1');
+runningDiv.append(p1);
+p1.style.color = " Red";
+p1.style.backgroundColor = "Yellow";
+p1.style.fontWeight = "bold";
+p1.style.fontSize = "30px";
 
 
 
@@ -36,8 +70,8 @@ let computerScore = 0;
 let gameChoices = ['Rock','Paper','Scissor']
 
 // user input and convert to lower case
-let playerSelection = prompt("User selection!");
-playerSelection = playerSelection.toLowerCase();
+//let playerSelection = prompt("User selection!");
+//playerSelection = playerSelection.toLowerCase(); unecessary for program since selections are button based
 
 const computerSelection = getComputerChoice();
 
@@ -50,13 +84,14 @@ return(gameChoices[(Math.random() * gameChoices.length) | 0].toLowerCase());
 
 
 function playRound(playerSelection,computerSelection){
+
+
     if(playerSelection == "rock"){
         const p = document.createElement('p');
         if(computerSelection == "rock"){
             const p = document.createElement('p');
             p.innerText = "it's a tie!";
-            outcomeDiv.appendChild(p);
-            
+            outcomeDiv.appendChild(p);      
             
         }
         else if ( computerSelection == "paper"){
@@ -71,8 +106,9 @@ function playRound(playerSelection,computerSelection){
             outcomeDiv.appendChild(p);
             playerScore = playerScore+1;
         }
-
-
+        p1.innerText = `Current Score: Player : ${playerScore} Computer : ${computerScore}`;
+        
+        
     }
     else if(playerSelection == "paper"){
         if(computerSelection == "rock"){
@@ -93,7 +129,8 @@ function playRound(playerSelection,computerSelection){
             computerScore = computerScore+1;
         }
 
-
+        p1.innerText = `Current Score: Player : ${playerScore} Computer : ${computerScore}`;
+     
     }
     
     else if(playerSelection == "scissor"){
@@ -115,7 +152,8 @@ function playRound(playerSelection,computerSelection){
             outcomeDiv.appendChild(p);
         }
 
-
+        p1.innerText = `Current Score: Player : ${playerScore} Computer : ${computerScore}`;
+        
     }
     
 
